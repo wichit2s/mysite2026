c2ebdb2c7f0be3a656ba44f9317e3cd100570948
thesombats <thesombats@gmail.com>
2026-07-02 15:35:09 +0700

    feat: add lucksoot PDF document and define initial project URL routing with view functions

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,39 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.utils import datastructures
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse, JsonResponse, FileResponse
+from django.shortcuts import render
+from django.conf import settings
+
+def home(request):
+    return HttpResponse('Hello from home.')
+
+def info(request):
+    data = {}
+    print('request.META')
+    for k,v in request.META.items():
+        data[str(k)] = str(v)
+        print(k,'=', v)
+    return JsonResponse(data)
+
+def shopping(request):
+    return render(request, 'shopping.html')
+
+def lucksoot_pdf(request):
+    file_path = settings.BASE_DIR / 'lucksoot.pdf'
+    return FileResponse(
+        open(file_path, 'rb'), 
+        content_type='application/pdf', 
+        filename='lucksoot.pdf')
 
 urlpatterns = [
+    path('', home),
+    path('info/', info),
+    path('shopping/', shopping),
+    path('lucksoot/', lucksoot_pdf),
     path('admin/', admin.site.urls),
 ]
+
